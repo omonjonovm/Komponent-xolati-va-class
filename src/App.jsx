587458cf08112965f9367bf44b1a71e1fe5633be
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-04-26 20:04:20 +0500

    add new events

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ class User extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      counter: 0,
+      counter: 0,age:''
     }
   }
   clickHandle = () => {
@@ -21,21 +21,31 @@ class User extends Component {
       counter:0,
     })
   }
+  changeHandle = (e) => {
+   this.setState({
+    age:e.target.value,
+   })
+  }
   render() {
     const { firstname, lastName, link } = this.props
+    const {age,counter} = this.state
     return (
       <div className="w-50 mx-auto">
         <div className="border p-3 mt-5">
           <h4>
-            Mening ismim - {firstname} sharifim - {lastName}
+            Mening ismim - {firstname} sharifim - {lastName}, yoshim - {age}
           </h4>
           <a href={link}>telegram sahifam</a>
           <div className="mt-3">
             <button onClick={this.clickHandle} className="btn btn-success">Increment</button>
             <button onClick={this.handleDelete}className="btn btn-danger mx-2">Decrement</button>
             <button onClick={this.handleRes}className="btn btn-info ">Restart</button>
-            <p>{this.state.counter}</p>
+            <p>{counter}</p>
           </div>
+          <form>
+            <span>Yoshingiz</span>
+            <input type="number" className="form-control" onChange={this.changeHandle}/>
+          </form>
         </div>
       </div>
     )
